01c9ae4cc5992435671d145945330a7653d98269
try <try@try>
2016-12-25 14:58:25 +0530

    Initial commit

---
+++ src/Asterik.java
@@ -0,0 +1,15 @@
+/**
+ * Created by jvishal on 12/25/16.
+ */
+public class Asterik {
+
+    public String displayAsterik()
+    {
+        return "*";
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(new Asterik().displayAsterik());
+    }
+}
